26320de684c1547660afe4bf4fb017f2cd13e2fa
s1roj <sirojiddinturopov1@gmail.com>
2026-03-30 00:32:22 +0500

    clean commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,232 @@
+<template>
+  <div class="bg-[#EAEAEA]">
+    <div class="mx-auto max-w-[1920px]">
+      <div class="main pb-[220px] mb-[170px]">
+        <div class="container mx-auto py-5">
+          <div class="header flex justify-between items-center mb-[160px]">
+            <div class="left-side">
+              <ul class="flex gap-[40px] text-2xl">
+                <li>Home</li>
+                <li>About Us</li>
+              </ul>
+            </div>
+            <h1 class="title text-4xl font-semibold">
+              Termiz davlat muhandislik va agrotexnologiyalar universiteti
+            </h1>
+            <div class="right-side">
+              <ul class="flex gap-[40px] text-2xl">
+                <li>Course</li>
+                <li>News</li>
+              </ul>
+            </div>
+          </div>
+          <div class="content">
+            <h1
+              class="text-[100px] font-bold max-w-[900px] text-center mx-auto mb-10"
+            >
+              Shaping the Future Through Education and Innovation
+            </h1>
+            <p class="mb-[60px] text-xl max-w-[900px] text-center mx-auto">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at
+              massa sit amet nisi blandit vehicula. Donec sollicitudin luctus
+              fermentum.
+            </p>
+            <div class="buttons flex justify-center gap-20">
+              <button
+                class="bg-blue-700 text-white px-4 py-2 rounded-full w-[270px] text-2xl"
+              >
+                Get Started
+              </button>
+              <button
+                class="bg-none border border-white px-4 py-2 rounded-full w-[270px] text-2xl"
+              >
+                Learn More
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section container mx-auto mb-[170px]">
+        <div class="texts flex justify-between items-center pb-10">
+          <h1
+            class="text-[#0540F2] text-[64px] font-semibold max-w-[750px] text-start"
+          >
+            Univerz's Commitment to Academic Excellence
+          </h1>
+          <p class="text-xl text-[#222222] max-w-[550px] text-left">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+            tristique, tortor nec consequat vulputate.
+          </p>
+        </div>
+        <div class="statistic grid grid-cols-10">
+          <div class="pr-[75px] col-span-4">
+            <img class="w-[530px]" src="../assets/Rectangle12.png" alt="" />
+          </div>
+          <div
+            class="max-w-[433px] flex flex-col justify-between col-span-4 mx-auto"
+          >
+            <img class="w-[433px]" src="../assets/Rectangle13.png" alt="" />
+            <p class="text-xl text-[#222222] text-start">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+              tristique, tor nec consequat vulputate, lorem ar congue.
+            </p>
+            <button
+              class="text-2xl py-3 px-16 bg-[#0540F2] rounded-full max-w-[270px]"
+            >
+              Read More
+            </button>
+          </div>
+          <div class="flex flex-col justify-between col-span-1 max-w-[185px]">
+            <div
+              class="p-[37px] rounded-2xl bg-[#FFFFFF] flex flex-col items-center"
+            >
+              <p class="blue text-[32px] font-bold">35+</p>
+              <p class="gray text-xl font-medium">Course</p>
+            </div>
+            <div
+              class="p-[37px] rounded-2xl bg-[#FFFFFF] flex flex-col items-center"
+            >
+              <p class="blue text-[32px] font-bold">56+</p>
+              <p class="gray text-xl font-medium">Startups</p>
+            </div>
+            <div
+              class="p-[37px] rounded-2xl bg-[#FFFFFF] flex flex-col items-center"
+            >
+              <p class="blue text-[32px] font-bold">100+</p>
+              <p class="gray text-xl font-medium">Language</p>
+            </div>
+            <div
+              class="p-[37px] rounded-2xl bg-[#FFFFFF] flex flex-col items-center"
+            >
+              <p class="blue text-[32px] font-bold">150+</p>
+              <p class="gray text-xl font-medium">Professors</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="course mx-auto container mb-[170px]">
+        <h1
+          class="text-[64px] font-bold blue max-w-[970px] mx-auto text-center mb-[40px]"
+        >
+          Explore Our Academic Offerings Chart Your Path to Success
+        </h1>
+        <p class="max-w-[640px] gray text-xl font-medium text-center mx-auto mb-[70px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at
+          massa sit amet nisi blandit vehicula adipiscing elit.
+        </p>
+        <div class="flex flex-wrap gap-[54px]">
+          <div
+            class="bg-[#0540F2] rounded-2xl flex max-w-[400px] items-center p-[37px] gap-[24px] mx-auto"
+          >
+            <div>
+              <img class="w-28" src="../assets/Icon1.png" alt="" />
+            </div>
+            <div>
+              <h3 class="font-semibold text-[32px]">Engineering</h3>
+              <p class="text-[16px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+          </div>
+          <div
+            class="bg-[#0540F2] rounded-2xl flex max-w-[400px] items-center p-[37px] gap-[24px] mx-auto"
+          >
+            <div>
+              <img class="w-28" src="../assets/Icon2.png" alt="" />
+            </div>
+            <div>
+              <h3 class="font-semibold text-[32px]">Psychology</h3>
+              <p class="text-[16px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+          </div>
+          <div
+            class="bg-[#0540F2] rounded-2xl flex max-w-[400px] items-center p-[37px] gap-[24px] mx-auto"
+          >
+            <div>
+              <img class="w-28" src="../assets/Icon3.png" alt="" />
+            </div>
+            <div>
+              <h3 class="font-semibold text-[32px]">Fine Arts</h3>
+              <p class="text-[16px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+          </div>
+          <div
+            class="bg-[#0540F2] rounded-2xl flex max-w-[400px] items-center p-[37px] gap-[24px] mx-auto"
+          >
+            <div>
+              <img class="w-28" src="../assets/Icon4.png" alt="" />
+            </div>
+            <div>
+              <h3 class="font-semibold text-[32px]">Language</h3>
+              <p class="text-[16px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+          </div>
+          <div
+            class="bg-[#0540F2] rounded-2xl flex max-w-[400px] items-center p-[37px] gap-[24px] mx-auto"
+          >
+            <div>
+              <img class="w-28" src="../assets/Icon5.png" alt="" />
+            </div>
+            <div>
+              <h3 class="font-semibold text-[32px]">Accountancy</h3>
+              <p class="text-[16px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+          </div>
+          <div
+            class="bg-[#0540F2] rounded-2xl flex max-w-[400px] items-center p-[37px] gap-[24px] mx-auto"
+          >
+            <div>
+              <img class="w-28" src="../assets/Icon6.png" alt="" />
+            </div>
+            <div>
+              <h3 class="font-semibold text-[32px]">Science</h3>
+              <p class="text-[16px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="flex justify-center mt-[80px]">
+          <button
+            class="rounded-full px-[85px] py-[12px] text-2xl font-medium bg-[#0540f2]"
+          >
+            View All
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  background-image: url("../assets/Group.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  text-decoration: none;
+  color: white;
+}
+.white {
+  color: white;
+}
+.blue {
+  color: #0540f2;
+}
+.gray {
+  color: #222222;
+}
+</style>
